4eecc495dbcde5945435ba1e109428bcd503c968
Brian Klarman <bklar84@gmail.com>
2020-12-26 15:01:33 -0500

    Remove and update references from Vue template after initialization

---
+++ frontend/billtracking/src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <h2>Bill Tracking</h2>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
